1ce51820b649968cc0edc5407438e8232cbc68cb
raghda-ali <raghdaali991@gmail.com>
2020-04-14 04:48:09 +0200

    .

--- E_market/App_Start/RouteConfig.cs
+++ E_market/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace E_market
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
