98688a7ebc0f1791a83ab76cb1b84a71fd09c26e
raghda-ali <raghdaali991@gmail.com>
2020-05-03 16:22:57 +0200

    add Cart Table

---
+++ E_market/Migrations/202005031419599_addingCartTable.cs
@@ -0,0 +1,30 @@
+namespace E_market.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addingCartTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Carts",
+                c => new
+                    {
+                        product_id = c.Int(nullable: false),
+                        added_at = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.product_id)
+                .ForeignKey("dbo.Products", t => t.product_id)
+                .Index(t => t.product_id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Carts", "product_id", "dbo.Products");
+            DropIndex("dbo.Carts", new[] { "product_id" });
+            DropTable("dbo.Carts");
+        }
+    }
+}
