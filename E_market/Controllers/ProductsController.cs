75846c1b6de21b4d7836f2252f36b6247b281444
ririelgamal <ririelgamal@gmail.com>
2020-05-11 22:29:38 +0400

    .

--- E_market/Controllers/ProductsController.cs
+++ E_market/Controllers/ProductsController.cs
@@ -209,18 +209,18 @@ namespace E_market.Controllers
             return Redirect("Index");
         }
 
-        public string calculated(DateTime added)
-        {
-            DateTime current = DateTime.Now;
-            TimeSpan diff = current.Subtract(added);
-            int time = diff.Hours;
-            if (time < 1) { time = diff.Minutes; return time.ToString() + "Minute"; }
-            else if (time >=1 && time <24) { return time.ToString() + "hours"; }
-            else if (time >= 24 && time < 168) { time = diff.Days; return time.ToString() + "day"; }
-            else { time = time / 168; return time.ToString() + "week"; }
+        //public string calculated(DateTime added)
+        //{
+        //    DateTime current = DateTime.Now;
+        //    TimeSpan diff = current.Subtract(added);
+        //    int time = diff.Hours;
+        //    if (time < 1) { time = diff.Minutes; return time.ToString() + "Minute"; }
+        //    else if (time >=1 && time <24) { return time.ToString() + "hours"; }
+        //    else if (time >= 24 && time < 168) { time = diff.Days; return time.ToString() + "day"; }
+        //    else { time = time / 168; return time.ToString() + "week"; }
  
 
-        }
+        //}
 
     }
 }
--- E_market/Models/Cart.cs
+++ E_market/Models/Cart.cs
@@ -14,5 +14,17 @@ namespace E_market.Models
         public int product_id { get; set; }
         public DateTime added_at { get; set; }
         public virtual Product Product { get; set; }
+        public string calculated(DateTime added)
+        {
+            DateTime current = DateTime.Today;
+            TimeSpan diff = current.Subtract(added);
+            int time = diff.Hours;
+            if (time < 1) { time = diff.Minutes; return time.ToString() + "Minute"; }
+            else if (time >= 1 && time < 24) { return time.ToString() + "hours"; }
+            else if (time >= 24 && time < 168) { time = diff.Days; return time.ToString() + "day"; }
+            else { time = time / 168; return time.ToString() + "week"; }
+
+
+        }
     }
 }
